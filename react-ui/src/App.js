0c9e0b19fe2a7c7a221056cb3182a7cea8dfe2a5
AdvaitChirmule <achirmul@asu.edu>
2024-02-04 15:00:58 -0700

    Third Commit

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="mainbody">
-        <h1 align="left">Login Page: </h1><br/>
-        <h5 align="left">Email address: </h5>
-        <input type="email" class="form-control" id="Input1" aria-describedby="emailHelp" placeholder="Enter email"></input><br/>
-        <h5 align="left">Password: </h5>
-        <input type="password" class="form-control" id="Input2" placeholder="Password"></input><br/>
-        <button type="button" className="btn btn-primary" id="Submit1">Submit</button>
-      </div>
+    <div className="mainbody">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email Address: </Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password: </Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Row className="justify-content-md-center">
+          <Col xs lg="2">
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </Form>
     </div>
   );
 }
