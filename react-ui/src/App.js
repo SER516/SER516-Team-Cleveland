cc1c8b90f4d85a11b4c72f6476e3673eb9b25aa7
spark290 <spark290@asu.edu>
2024-02-05 20:31:24 -0700

    US12Task42 Rebased branch, Connect Project Slug page from Login page, and modified as PR review

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,80 +1,31 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Button, Form, Row, Col } from "react-bootstrap";
-import './App.css';
-
-function App() {
-  return (
-    <div className="d-flex align-items-center justify-content-center vh-100">
-      <Form style={{ width: '100%' }}>
-        <Form.Group className="mb-3 col-sm-8 offset-sm-2" controlId="formUsername">
-          <Form.Label>Username: </Form.Label>
-          <Form.Control type="text" placeholder="Enter Username" />
-        </Form.Group>
-
-        <Form.Group className="mb-3 col-sm-8 offset-sm-2" controlId="formPassword">
-          <Form.Label>Password: </Form.Label>
-          <Form.Control type="password" placeholder="Enter Password" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit" className="col-sm-1 offset-sm-6">
-          Submit
-        </Button>
-      </Form>
-=======
 import React, { useState } from 'react';
-=======
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
->>>>>>> 4042ed0 (US12Task42 Modified HTML based code to React BootStrap based code and chaged the filename)
-=======
-import React, { useState } from 'react';
->>>>>>> 7bbe6ca (US12Task42 Added Clickon button effect for redirection page and modified css file for ProjectSlug.js)
-import logo from './logo.svg';
+import { Button, Form } from "react-bootstrap";
 import './App.css';
 import ProjectSlug from './ProjectSlug';
-import Button from 'react-bootstrap/Button';
 
 function App() {
   const [showProjectSlug, setShowProjectSlug] = useState(false);
 
   return (
-    <div className="App">
+    <div className="d-flex align-items-center justify-content-center vh-100">
       {showProjectSlug ? (
         <ProjectSlug />
       ) : (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Button variant="info" onClick={() => setShowProjectSlug(true)}>Go to Project Slug</Button>
-        </header>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      )}
->>>>>>> 778614a (US12Task42 Initial version of ProjectSelection.js and ProjectSelection.css)
-    </div>
-=======
-        <Routes>
-          <Route path="/ProjectSlug" element={<ProjectSlug />} />
-        </Routes>
+        <Form style={{ width: '100%' }}>
+          <Form.Group className="mb-3 col-sm-8 offset-sm-2" controlId="formUsername">
+            <Form.Label>Username: </Form.Label>
+            <Form.Control type="text" placeholder="Enter Username" />
+          </Form.Group>
+
+          <Form.Group className="mb-3 col-sm-8 offset-sm-2" controlId="formPassword">
+            <Form.Label>Password: </Form.Label>
+            <Form.Control type="password" placeholder="Enter Password" />
+          </Form.Group>
+
+          <Button variant="info" className="SubmitButton" onClick={() => setShowProjectSlug(true)}>Submit</Button>
+        </Form>
+          )}
       </div>
-    </Router>
->>>>>>> 4042ed0 (US12Task42 Modified HTML based code to React BootStrap based code and chaged the filename)
-=======
-      )}
-    </div>
->>>>>>> 7bbe6ca (US12Task42 Added Clickon button effect for redirection page and modified css file for ProjectSlug.js)
   );
 }
 
--- react-ui/src/ProjectSlug.js
+++ react-ui/src/ProjectSlug.js
@@ -1,14 +1,14 @@
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form';
 import React from 'react';
+import { Button, Form } from "react-bootstrap";
 import './ProjectSlug.css';
 
 function ProjectSlug() {
     return (
       <div className='projectSlugContainer'>
-        <Form>
+        <Form style={{ width: '100%' }}>
+          <Form.Label>Enter Project Slug</Form.Label>
           <Form.Control as="textarea" className="customTextarea" placeholder="Enter Project Slug" />
-          <Button variant="info" className="customButton">Submit</Button>
+          <Button variant="info" className="SubmitButton">Submit</Button>
         </Form>
       </div>
     );
