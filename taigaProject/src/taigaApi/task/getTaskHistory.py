9f8b80c4e12e4591a8d3f183950523ec5c8f3325
sverma89 <sverma89@asu.edu>
2024-02-09 17:08:16 -0700

    added code to calculate lead time for tasks

--- taigaProject/src/taigaApi/task/getTaskHistory.py
+++ taigaProject/src/taigaApi/task/getTaskHistory.py
@@ -2,6 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 from datetime import datetime
+from getTasks import get_closed_tasks
 
 # Load environment variables from .env file
 load_dotenv()
@@ -53,6 +54,27 @@ def get_task_history(tasks, auth_token):
     return [cycle_time, closed_tasks]
 
 
+def get_lead_time(project_id, auth_token):
+    tasks = get_closed_tasks(project_id, auth_token)
+    lead_time = 0
+    closed_tasks = 0
+    lead_times = []
+    for task in tasks:
+
+        created_date = datetime.fromisoformat(task["created_date"])
+        finished_date = datetime.fromisoformat(task['finished_date'])
+        lead_time += (finished_date - created_date).days
+        lead_times.append({
+            "taskId": task["id"],
+            "startTime": task["created_date"],
+            "endTime": task['finished_date'],
+            "timeTaken": lead_time
+        })
+        closed_tasks += 1
+
+    avg_lead_time = round((lead_time / closed_tasks), 2)
+
+    return lead_times, avg_lead_time
 # Function to extract the date when a task transitioned from 'New' to 'In progress'
 def extract_new_to_in_progress_date(history_data):
     for event in history_data:
