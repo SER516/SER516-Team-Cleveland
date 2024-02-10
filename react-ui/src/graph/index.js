c72da6952dccff1873a3fced6a56a663c3ae5daa
spark290 <spark290@asu.edu>
2024-02-09 22:15:16 -0700

    US15Task48 Added function and implementation of Average ReferenceLine, modified XAxis marking style and overall graph UI

--- react-ui/src/graph/index.js
+++ react-ui/src/graph/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine } from 'recharts';
 
 const Graph = () => {
     // Sample data
     const data = [
+        {name: 'Jan 8th', Userstory: 18, Task: 35},
+        {name: 'Jan 15th', Userstory: 24, Task: 28},
+        {name: 'Jan 22th', Userstory: 16, Task: 24},
         {name: 'Jan 29th', Userstory: 10, Task: 20},
         {name: 'Feb 5th', Userstory: 30, Task: 15},
         {name: 'Feb 12th', Userstory: 20, Task: 13},
@@ -13,6 +16,10 @@ const Graph = () => {
         {name: 'Mar 11th', Userstory: 34, Task: 5},
     ];
 
+    // Calculate Average
+    const avgUserstory = data.reduce((acc, cur) => acc + cur.Userstory, 0) / data.length;
+    const avgTask = data.reduce((acc, cur) => acc + cur.Task, 0) / data.length;
+
     return (
         <div style={{ textAlign: 'center' }}>
             <h2 style={{ marginBottom: '70px' }}>Lead Time</h2>
@@ -21,15 +28,18 @@ const Graph = () => {
                     <h3 style={{ textAlign: 'center' }}>Userstory</h3>
                     <ResponsiveContainer height={600}>
                         <ScatterChart
-                            margin={{top: 20, right: 20, bottom: 20, left: 20}}
+                            margin={{top: 20, right: 40, bottom: 70, left: 30}}
                         >
                             <CartesianGrid />
                             <XAxis type="category" dataKey="name" name="Date" label={{ value: 'Date', 
-                                    position: 'insideBottom', offset: -20, style: {fontSize: '20px'}}} />
+                                    position: 'insideBottom', offset: -50, style: {fontSize: '20px'}}} 
+                                    tick={{ angle: -45, textAnchor: 'end', fontSize: 12}}/>
                             <YAxis type="number" dataKey="Userstory" name="Userstory" label={{ value: 'Points', 
                                     angle: -90, position: 'insideLeft', style: {fontSize: '20px'}}} />
                             <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                             <Scatter name="Userstory" data={data} fill="#8884d8" />
+                            <ReferenceLine y={avgUserstory} stroke="red" strokeDasharray="3 3" strokeWidth={2}
+                                            label={{ value: 'Average', position: 'bottom', fontSize: 15, offset: 10 }} />
                         </ScatterChart>
                     </ResponsiveContainer>
                 </div>
@@ -38,15 +48,18 @@ const Graph = () => {
                     <h3 style={{ textAlign: 'center' }}>Task</h3>
                     <ResponsiveContainer height={600}>
                         <ScatterChart
-                            margin={{top: 20, right: 20, bottom: 20, left: 20}}
+                            margin={{top: 20, right: 30, bottom: 70, left: 40}}
                         >
                             <CartesianGrid />
                             <XAxis type="category" dataKey="name" name="Date" label={{ value: 'Date', 
-                                    position: 'insideBottom', offset: -20, style: {fontSize: '20px'}}} />
+                                    position: 'insideBottom', offset: -50, style: {fontSize: '20px'}}}
+                                    tick={{ angle: -45, textAnchor: 'end', fontSize: 12}}/>
                             <YAxis type="number" dataKey="Task" name="Tasks" label={{ value: 'Count', 
                                     angle: -90, position: 'insideLeft', style: {fontSize: '20px'}}} />
                             <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                             <Scatter name="Tasks" data={data} fill="#82ca9d" />
+                            <ReferenceLine y={avgTask} stroke="red" strokeDasharray="3 3" strokeWidth={2}
+                                            label={{ value: 'Average', position: 'bottom', fontSize: 15, offset: 10 }} />
                         </ScatterChart>
                     </ResponsiveContainer>
                 </div>
