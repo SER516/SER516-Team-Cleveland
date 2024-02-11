11c62687fd9d08a3065cc11d995a44d43a848913
achaur11 <143744608+achaur11@users.noreply.github.com>
2024-02-10 19:05:23 -0700

    US13-Task46: Adding API to fetch lead time details (#17)
    * US-13 Task-46: Fixing unit tests
    
    * US-13 Task-46: Minor fix

--- taigaProject/src/main.py
+++ taigaProject/src/main.py
@@ -3,10 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from taigaApi.controller import project_controller
 from taigaApi.controller import auth_controller
+from taigaApi.controller import metric_controller
 
 app = FastAPI()
 app.include_router(project_controller.router)
 app.include_router(auth_controller.router)
+app.include_router(metric_controller.router)
 
 origins = ["http://localhost:3000"]
 
