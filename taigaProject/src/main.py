7db4f80146e5cbde97fa1d9558d18350560d21a7
sverma89 <sverma89@asu.edu>
2024-02-13 18:34:17 -0700

    added API to get sprint details and custom attributes using project slug

--- taigaProject/src/main.py
+++ taigaProject/src/main.py
@@ -4,11 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from taigaApi.controller import project_controller
 from taigaApi.controller import auth_controller
 from taigaApi.controller import metric_controller
+from taigaApi.controller import sprint_controller
 
 app = FastAPI()
 app.include_router(project_controller.router)
 app.include_router(auth_controller.router)
 app.include_router(metric_controller.router)
+app.include_router(sprint_controller.router)
 
 origins = ["http://localhost:3000"]
 
