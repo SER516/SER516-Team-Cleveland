e416cf66b760de835136f6ddd5bae61c78a7ce55
sverma89 <sverma89@asu.edu>
2024-02-15 21:04:59 -0700

    fixed indentation

--- react-ui/src/areachart/index.js
+++ react-ui/src/areachart/index.js
@@ -21,12 +21,12 @@ const Areachart = ({ apiData, chartFor, title }) => {
                         >
                             <defs>
                                 <linearGradient id="colorExpected" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="#D04848" stopOpacity={0.8}/>
-                                <stop offset="95%" stopColor="#D04848" stopOpacity={0.1}/>
+                                    <stop offset="5%" stopColor="#D04848" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="#D04848" stopOpacity={0.1}/>
                                 </linearGradient>
                                 <linearGradient id="colorActual" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="#6895D2" stopOpacity={0.8}/>
-                                <stop offset="95%" stopColor="#6895D2" stopOpacity={0.1}/>
+                                    <stop offset="5%" stopColor="#6895D2" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="#6895D2" stopOpacity={0.1}/>
                                 </linearGradient>
                             </defs>
                             <CartesianGrid />
