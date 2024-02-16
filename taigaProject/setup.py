5d7716484f5b31a94ec8246558c66672c9cf4e39
sverma89 <sverma89@asu.edu>
2024-02-16 12:30:22 -0700

    added setup.py file

---
+++ taigaProject/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='taigaProject',
+    version='',
+    packages=['src', 'src.taigaApi', 'src.taigaApi.auth', 'src.taigaApi.task', 'src.taigaApi.util',
+              'src.taigaApi.project', 'src.taigaApi.userStory', 'src.taigaApi.controller', 'test', 'test.taigaApi'],
+    url='',
+    license='',
+    author='shikhaverma',
+    author_email='',
+    description=''
+)
