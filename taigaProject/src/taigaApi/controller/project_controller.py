507bbb3d299180774a1c89e3c2f8da0a9334bbe6
shahih25 <hmshahid@asu.edu>
2024-02-25 02:15:08 -0700

    US22Task76 Fetch members from project slug

--- taigaProject/src/taigaApi/controller/project_controller.py
+++ taigaProject/src/taigaApi/controller/project_controller.py
@@ -1,28 +1,23 @@
 from fastapi import FastAPI, Request, APIRouter, HTTPException, Header
 from typing import Annotated
 
-from taigaApi.model.projectRequest import ProjectRequest
+from ..model.projectRequest import ProjectRequest
 
-from taigaApi.project.getProjectBySlug import get_project_by_slug
-from taigaApi.project.getProjectTaskStatusName import get_project_task_status_name
+from ..project.getProjectBySlug import get_project_by_slug
+from ..util.project_service import get_project_members
 
 router = APIRouter()
 
 @router.post("/Project")
 def auth(projectRequest: ProjectRequest, token: Annotated[str | None, Header()] = None):
+    print(projectRequest)
     project_info = get_project_by_slug(projectRequest.projectslug, token)
+    print(project_info)
     if project_info is None:
         raise HTTPException(status_code=404, detail="Project Slug Not Found.")
-    task_status_name = get_project_task_status_name(project_info["id"], token)
 
     project_details = {
         "name": project_info["name"],
-        "team_members": [member["full_name"] for member in project_info.get("members", [])],
-        "taskboard_column": [status["name"] for status in task_status_name],
+        "team_members": get_project_members(project_info)
     }
     return project_details
-
-
-
-
-
