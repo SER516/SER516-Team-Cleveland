99f4ad57b579fe27bf471bee39127f3e8530ba39
spark290 <spark290@asu.edu>
2024-02-27 18:40:57 -0700

    US23Task79 Visualize using barchart

--- react-ui/src/graph/barchart.js
+++ react-ui/src/graph/barchart.js
@@ -19,71 +19,46 @@ const CustomBarChart = ({ title }) => {
         { date: '2024-02-05', User1: 2, User2: 3, User3: 3, User4: 2, User5: 2  },
     ];
 
-// const CustomBarChart = ({ apiData, title }) => {
-    // const CustomToolTip = ({ active, payload }) => {
-    //     if (active && payload && payload.length) {
-    //         return (
-    //             <div className="custom-tooltip" style={{ backgroundColor: '#ffd7b5', padding: '5px', border: '1px solid #ccc', borderRadius: "5px"}}>
-    //                 <b>Date:</b> {payload[0].value}<br/>
-    //                 <b>Days:</b> {payload[1].value}<br/>
-    //                 <b>Task ID:</b> {payload[1].payload.taskRef}<br/>
-    //                 <b>Task Description:</b> {payload[1].payload.taskDesc}<br/>
-    //             </div>
-    //         );
-    //     }
-    //     return null;
-    // };
-
     return (
         <div>
-            <div>
-                <h4 style={{ textAlign: 'center' }}>{title}</h4>
-                <ResponsiveContainer height={1250}>
-                    <div>
-                        <BarChart
-                            width={1400}
-                            height={600}
-                            data={dummyData}  // For dummy Test
-                            margin={{ top: 20, right: 40, bottom: 70, left: 30 }}
-                        >    
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis type="category" dataKey="name" label={{
-                                value: 'User', position: 'insideBottom', offset: -50, style: { fontSize: '20px' }
-                            }}
-                                tick={{ fontSize: 12 }} allowDuplicatedCategory={false} />
-                            <YAxis type="number" dataKey="" label={{
-                                value: 'Counts', angle: -90, position: 'insideLeft', style: { fontSize: '20px' }
-                            }} />
-                            {/*<Tooltip content={<CustomToolTipTop />} />*/}
-                            <BarChart name={title} data={dummyData} fill="#8884d8" />
-                            <Bar dataKey="counts" fill="#8884d8" />
-                        </BarChart>
-                        <div style={{ height: 20}} />
-                        <BarChart
-                            width={1400}
-                            height={600}
-                            data={stackedData}
-                            margin={{ top: 20, right: 40, bottom: 70, left: 30 }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis type="category" dataKey="date" label={{value: 'Date',
-                                position: 'insideBottom', offset: -50, style: { fontSize: '20px' }
-                            }}
-                                tick={{ fontSize: 12 }} allowDuplicatedCategory={false} />
-                            <YAxis type="number" dataKey="" label={{
-                                value: 'Counts', angle: -90, position: 'insideLeft', style: { fontSize: '20px' }
-                            }} />
-                            {/*<Tooltip content={<CustomToolTipBottom />} />*/}
-                            <Legend align="right" verticalAlign="top" layout="horizontal" iconType="square"/>
-                            <Bar dataKey="User1" stackId="a" fill="#8884d8" />
-                            <Bar dataKey="User2" stackId="a" fill="#82ca9d" />
-                            <Bar dataKey="User3" stackId="a" fill="#ffc658" />
-                            <Bar dataKey="User4" stackId="a" fill="#d45087" />
-                            <Bar dataKey="User5" stackId="a" fill="#8e7cc3" />
-                    </BarChart>
-                </div>
-                </ResponsiveContainer>
-            </div>
+            <h4 style={{ textAlign: 'center' }}>{title}</h4>
+            <ResponsiveContainer width="100%" height={600}>
+                <BarChart
+                    data={dummyData}  // For dummy Test
+                    margin={{ top: 20, right: 40, bottom: 70, left: 30 }}
+                >    
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis type="category" dataKey="name" label={{
+                        value: 'User', position: 'insideBottom', offset: -50, style: { fontSize: '20px' }
+                    }}
+                        tick={{ fontSize: 12 }} allowDuplicatedCategory={false} />
+                    <YAxis type="number" dataKey="" label={{
+                        value: 'Counts', angle: -90, position: 'insideLeft', style: { fontSize: '20px' }
+                    }} />
+                    <Bar dataKey="counts" fill="#8884d8" />
+                </BarChart>
+            </ResponsiveContainer>
+            <ResponsiveContainer width="100%" height={600}>
+                <BarChart
+                    data={stackedData}
+                    margin={{ top: 20, right: 40, bottom: 70, left: 30 }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis type="category" dataKey="date" label={{value: 'Date',
+                        position: 'insideBottom', offset: -50, style: { fontSize: '20px' }
+                    }}
+                        tick={{ fontSize: 12 }} allowDuplicatedCategory={false} />
+                    <YAxis type="number" dataKey="" label={{
+                        value: 'Counts', angle: -90, position: 'insideLeft', style: { fontSize: '20px' }
+                    }} />
+                    <Legend align="right" verticalAlign="top" layout="horizontal" iconType="square"/>
+                    <Bar dataKey="User1" stackId="a" fill="#8884d8" />
+                    <Bar dataKey="User2" stackId="a" fill="#82ca9d" />
+                    <Bar dataKey="User3" stackId="a" fill="#ffc658" />
+                    <Bar dataKey="User4" stackId="a" fill="#d45087" />
+                    <Bar dataKey="User5" stackId="a" fill="#8e7cc3" />
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 }
