f7ef8dbe2ecc6c3b3fce6a0b3e5370b64dce57e3
achaur11 <achaur11@asu.edu>
2024-03-01 02:21:29 -0700

    US-22: Adding Dev focus related backend changes

--- taigaProject/src/taigaApi/task/get_task_history.py
+++ taigaProject/src/taigaApi/task/get_task_history.py
@@ -4,7 +4,7 @@ import requests
 from dotenv import load_dotenv
 from datetime import datetime
 
-from .getTasks import get_closed_tasks
+from .getTasks import get_closed_tasks, task_by_member_in_date_range
 
 # Load environment variables from .env file
 load_dotenv()
@@ -12,7 +12,6 @@ load_dotenv()
 
 # Function to retrieve task history and calculate cycle time for closed tasks
 def get_task_history(tasks, auth_token):
-
     # Get Taiga API URL from environment variables
     taiga_url = os.getenv('TAIGA_URL')
 
@@ -124,6 +123,20 @@ def extract_new_to_in_progress_date(history_data):
     return None
 
 
+def extract_state_change_dates(history_data):
+    in_progress_date, closed_at = None, None
+    for event in history_data:
+        values_diff = event.get("values_diff", {})
+        if "status" in values_diff:
+            if values_diff["status"] == ["New", "In progress"]:
+                in_progress_date = datetime.fromisoformat(event["created_at"])
+                print("In the new -> IP loop")
+            if values_diff["status"] == ['In progress', 'DONE']:
+                closed_at = datetime.fromisoformat(event["created_at"])
+                print("In the IP -> Done loop")
+    return in_progress_date, closed_at
+
+
 def get_task_cycle_time(project_id, auth_token):
     tasks = get_closed_tasks(project_id, auth_token)
     taiga_url = os.getenv('TAIGA_URL')
@@ -146,3 +159,122 @@ def get_task_cycle_time(project_id, auth_token):
 
     avg_cycle_time = round((cycle_time_data["cycle_time"] / cycle_time_data["closed_tasks"]), 2)
     return cycle_times, avg_cycle_time
+
+
+def dev_focus_tasks(task, headers, taiga_url, from_date, to_date, dev_focus_data):
+    task_history_url = f"{taiga_url}/history/task/{task['id']}"
+    try:
+        response = requests.get(task_history_url, headers=headers)
+        response.raise_for_status()
+        history_data = response.json()
+        in_progress_date = extract_new_to_in_progress_date(history_data)
+        print(in_progress_date == None)
+
+        if in_progress_date:
+            in_progress_date = datetime.fromisoformat(str(in_progress_date)[:-6])
+
+            if in_progress_date > datetime.fromisoformat(from_date) and in_progress_date < datetime.fromisoformat(
+                    to_date):
+                dev_focus_data.append({
+                    "taskId": task["id"],
+                    "taskDesc": task["subject"],
+                    "taskRef": task["ref"],
+                    "created_date": task["created_date"],
+                    "inProgressDate": in_progress_date.date(),
+                    "username": task["username"],
+                    "full_name": task["full_name"]
+                })
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching task by taskId: {e}")
+
+
+def get_all_dev_focus(project_id, from_date, to_date, threshold, members, auth_token):
+    tasks = get_dev_focus(project_id, from_date, to_date, threshold, members, auth_token)
+    taiga_url = os.getenv('TAIGA_URL')
+
+    headers = {
+        'Authorization': f'Bearer {auth_token}',
+        'Content-Type': 'application/json',
+        "x-disable-pagination": "True"
+    }
+
+    dev_focus_data = []
+    with ThreadPoolExecutor(max_workers=15) as executor:
+        for task in tasks:
+            executor.submit(dev_focus_tasks, task, headers, taiga_url,
+                            from_date, to_date, dev_focus_data)
+
+    return dev_focus_data
+
+
+def get_dev_focus(project_id, from_date, to_date, threshold, members, auth_token):
+    all_tasks = []
+    for member in members:
+        tasks = task_by_member_in_date_range(project_id, member, from_date, to_date, auth_token)
+        all_tasks = all_tasks + tasks
+
+    return all_tasks
+
+
+def member_tasks(project_id, from_date, to_date, members, auth_token):
+    member_tasks = {}
+    # for member in members:
+    # member_map(project_id, from_date, to_date, member, auth_token, member_tasks)
+    with ThreadPoolExecutor(max_workers=15) as executor:
+        for member in members:
+            executor.submit(member_map, project_id, from_date, to_date, member, auth_token, member_tasks)
+
+    #for key in member_tasks:
+        #member_tasks[key].sort(key=lambda l: l["inProgressDate"])
+    return member_tasks
+
+
+def member_map(project_id, from_date, to_date, member, auth_token, member_tasks):
+    tasks = task_by_member_in_date_range(project_id, member, from_date, to_date, auth_token)
+
+    taiga_url = os.getenv('TAIGA_URL')
+
+    headers = {
+        'Authorization': f'Bearer {auth_token}',
+        'Content-Type': 'application/json',
+        "x-disable-pagination": "True"
+    }
+
+    with ThreadPoolExecutor(max_workers=15) as executor:
+        for task in tasks:
+            executor.submit(sorted_tasks_data, task, headers, taiga_url,
+                            from_date, to_date, member_tasks)
+
+
+def sorted_tasks_data(task, headers, taiga_url, from_date, to_date, member_tasks):
+    task_history_url = f"{taiga_url}/history/task/{task['id']}"
+    try:
+        response = requests.get(task_history_url, headers=headers)
+        response.raise_for_status()
+        history_data = response.json()
+        in_progress_date, closed_date = extract_state_change_dates(history_data)
+        if (in_progress_date is not None or closed_date is not None):
+            in_progress_date = datetime.fromisoformat(str(in_progress_date)[:-6]) if in_progress_date is not None else None
+            closed_date = datetime.fromisoformat(str(closed_date)[:-6]) if closed_date is not None else None
+            if ((in_progress_date is not None and datetime.fromisoformat(from_date) < in_progress_date < datetime.fromisoformat(
+                    to_date))) or ( closed_date is not None and datetime.fromisoformat(from_date) < closed_date < datetime.fromisoformat(
+                to_date)):
+                task_data = {
+                    "taskId": task["id"],
+                    "taskDesc": task["subject"],
+                    "taskRef": task["ref"],
+                    "created_date": task["created_date"],
+                    "inProgressDate": in_progress_date,
+                    "closed_date": closed_date,
+                    "finished_date": task["finished_date"],
+                    "username": task["username"],
+                    "full_name": task["full_name"]
+                }
+                if task["username"] in member_tasks:
+                    member_tasks[task["username"]].append(task_data)
+                else:
+                    member_tasks[task["username"]] = [task_data]
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching task by taskId: {e}")
