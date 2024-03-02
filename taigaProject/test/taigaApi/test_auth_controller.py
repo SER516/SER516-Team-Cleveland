80c216798a18078847b4b60aff0a599cb0bc8b89
shahih25 <hmshahid@asu.edu>
2024-03-01 20:09:45 -0700

    hotfix-tests update

--- taigaProject/test/taigaApi/test_auth_controller.py
+++ taigaProject/test/taigaApi/test_auth_controller.py
@@ -13,7 +13,7 @@ def token():
     return "token"
 
 @patch('os.getenv', MagicMock(return_value='http://fake.taiga.url'))
-@patch("requests.post")
+@patch("requests.get")
 def test_auth(mock_get, token):
     mock_response = MagicMock()
     mock_response.json.return_value = token
