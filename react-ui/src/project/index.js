a4cf7cd4651f7c239dc37bd8fa71436510205240
spark290 <spark290@asu.edu>
2024-03-18 22:08:18 -0700

    [US107Task142] Modified after PR review

--- react-ui/src/project/index.js
+++ react-ui/src/project/index.js
@@ -199,8 +199,7 @@ const Project = () => {
                         </div>
                     ) : null}
                     {selectedValue === "Burndown Chart" && isBurndown ? (
-                        // <SprintDetail sprintDetails={data.sprints} attributes={data.custom_attributes} token={auth} projectName={data.name} />
-                        <Graph type="Burndown Chart" />
+                        <SprintDetail sprintDetails={data.sprints} attributes={data.custom_attributes} token={auth} projectName={data.name} />
                     ) : null}
                     {selectedValue === "Dev Focus" && isDevFocus ? (
                         <DateSelector memberDetails={data.members} token={auth} projectId={data.id} onDateSubmit={(startDate, endDate) => {
