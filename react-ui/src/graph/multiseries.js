1191320596c4ae347eefc9073b67219145245f28
shahih25 <hmshahid@asu.edu>
2024-03-23 17:44:58 -0700

    [US108 Task155] Update the multiseries chart

--- react-ui/src/graph/multiseries.js
+++ react-ui/src/graph/multiseries.js
@@ -1,38 +1,7 @@
-import { useEffect, useState } from "react";
 import { Tooltip } from "react-bootstrap";
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 const CustomMultiSeriesLineChart = ({ apiData, title, chartType, dataKey }) => {
-
-    const series = [
-        {
-          name: 'Series 1',
-          data: [
-            { category: 'A', value: Math.random() },
-            { category: 'B', value: Math.random() },
-            { category: 'C', value: Math.random() },
-          ],
-        },
-        {
-          name: 'Series 2',
-          data: [
-            { category: 'B', value: Math.random() },
-            { category: 'C', value: Math.random() },
-            { category: 'D', value: Math.random() },
-          ],
-        },
-        {
-          name: 'Series 3',
-          data: [
-            { category: 'C', value: Math.random() },
-            { category: 'D', value: Math.random() },
-            { category: 'E', value: Math.random() },
-          ],
-        },
-      ];
-
-
-    const [sprintData, setSprintData] = useState(null);
     const colorGenerator = () => {
         let letters = "0123456789ABCDEF";
         let color = "#";
@@ -41,20 +10,6 @@ const CustomMultiSeriesLineChart = ({ apiData, title, chartType, dataKey }) => {
         }
         return color;
     }
-
-    useEffect(() => {
-        let data = [];
-        for (let d in apiData) {
-            let obj = {
-                data: apiData[d].combined_burndown.data,
-                sprint: apiData[d].sprint
-            };
-            console.log(obj);
-            data.push(obj);
-        }
-        console.log(data);
-        setSprintData(data);
-    }, [apiData]);
 
     return (
         <div style={{ textAlign: 'center' }}>
