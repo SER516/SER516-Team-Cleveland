664c47e1e49351fed77dd9e0b399a5e9dad6f119
shahih25 <hmshahid@asu.edu>
2024-03-24 15:54:06 -0700

    [US112] Remove a space

--- taigaProject/src/main.py
+++ taigaProject/src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-
 from taigaApi.controller import project_controller
 from taigaApi.controller import auth_controller
 from taigaApi.controller import metric_controller
