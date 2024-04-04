f10dbd7383374287977489cfde8568d3a727ad53
sverma89 <sverma89@asu.edu>
2024-04-03 19:52:31 -0700

    [US163 Task 181] added auth into the library

--- library/setup.py
+++ library/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='hmtaiga',
     packages=find_packages(),
-    version='0.1.6',
+    version='0.1.7',
     description='Taiga Library',
     author='shikhaverma'
 )
