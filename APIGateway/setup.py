7210e75c97845d2c0482405a303d29b18f8ef96e
achaur11 <achaur11@asu.edu>
2024-04-04 22:20:22 -0700

    [US170 Task185] Adding API Gateway folder structure

---
+++ APIGateway/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='APIGateway',
+    version='1.0',
+    packages=['src', 'src.gateway', 'src.gateway.controller', 'test'],
+    url='',
+    license='',
+    author='achaur11',
+    author_email='',
+    description=''
+)
