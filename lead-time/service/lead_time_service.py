b3fb332fc5c59bcfd6557da5047145e3c43a00a1
AdvaitChirmule <achirmul@asu.edu>
2024-04-05 03:08:33 -0700

    [US164 Task177] Fetch Tasks for Lead Time

--- lead-time/service/lead_time_service.py
+++ lead-time/service/lead_time_service.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 
 def get_lead_time_details(project_details,
@@ -12,13 +12,21 @@ def get_lead_time_details(project_details,
     us_lead_time, avg_us_lt = get_us_lead_time(
         project_details["id"], auth_token, from_date, to_date
     )
+    task_lead_time, avg_task_lt = get_task_lead_time(
+        project_details["id"], auth_token, from_date, to_date
+    )
 
     us_lead_time.sort(key=lambda item: item["endDate"])
+    task_lead_time.sort(key=lambda item: item["endDate"])
 
+    # get objects created that store the details
+    # for task and user story lead times
     us_lead_time = get_lead_time_object("userStory", us_lead_time, avg_us_lt)
+    task_lead_time = get_lead_time_object("task", task_lead_time, avg_task_lt)
 
     lead_time = {
         "storiesLeadTime": us_lead_time,
+        "tasksLeadTime": task_lead_time
     }
     return metric_object("LEAD", "leadTime", lead_time, project_details)
 
@@ -69,4 +77,35 @@ def get_us_lead_time(project_id, auth_token, from_date=None, to_date=None):
     if closed_user_stories == 0:
         return lead_times, 0
     avg_lead_time = round((lead_time / closed_user_stories), 2)
+    return lead_times, avg_lead_time
+
+def get_task_lead_time(project_id, auth_token, from_date=None, to_date=None):
+    tasks = get_closed_tasks(project_id, auth_token)
+    lead_time = 0
+    closed_tasks = 0
+    lead_times = []
+    for task in tasks:
+        created_date = datetime.fromisoformat(task["created_date"])
+        finished_date = datetime.fromisoformat(task['finished_date'])
+        if from_date is not None and from_date > finished_date.date():
+            continue
+        if to_date is not None and to_date < finished_date.date():
+            continue
+        lead_time += (finished_date - created_date).days
+        lead_times.append({
+            "taskDesc": task["subject"],
+            "sprintURL": task["sprintURL"],
+            "taskRef": task["ref"],
+            "taskId": task["id"],
+            "startTime": task["created_date"],
+            "startDate": created_date.date(),
+            "endTime": task['finished_date'],
+            "endDate": finished_date.date(),
+            "timeTaken": (finished_date - created_date).days
+        })
+        closed_tasks += 1
+    if closed_tasks == 0:
+        return lead_times, 0
+    avg_lead_time = round((lead_time / closed_tasks), 2)
+
     return lead_times, avg_lead_time
